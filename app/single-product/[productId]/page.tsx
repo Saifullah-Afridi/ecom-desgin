038645556d8b6d938bb506b825fa16a36aea94cc
Saifullah-Afridi <saifullahafridi08@gmail.com>
2026-01-21 22:32:45 +0500

    chagnes done

--- app/single-product/[productId]/page.tsx
+++ app/single-product/[productId]/page.tsx
@@ -4,33 +4,43 @@ import { Check, Star } from "lucide-react";
 export default function ProductDetails() {
   return (
     <div className="w-full max-w-7xl px-4 md:px-8 lg:px-20 mx-auto mt-30">
-      <div className="flex flex-col md:flex-row gap-4 mb-10">
-        <div className="w-full">
-          <img
-            src="/images/single-product-image.jpg"
-            alt="Product Image"
-            className="object-cover w-full"
-          />
-          <div className="flex gap-0.5 mt-1">
+      <div className="flex flex-col md:flex-row gap-4   mb-10">
+        <div className="w-full border-2">
+          <div className="w-full aspect-square border-2 ">
             <img
               src="/images/single-product-image.jpg"
               alt="Product Image"
-              className="w-[33%] object-cover"
-            />
-            <img
-              src="/images/single-product-image.jpg"
-              alt="Product Image"
-              className="w-[33%] object-cover"
-            />
-            <img
-              src="/images/single-product-image.jpg"
-              alt="Product Image"
-              className="w-[33%] object-cover"
+              className="object-cover w-full h-full "
             />
           </div>
+          <div className="flex gap-0.5 mt-1 border-2">
+            <div className="flex-1 aspect-square border-2 ">
+              <img
+                src="/images/single-product-image.jpg"
+                alt="Product Image"
+                className="object-cover w-full h-full "
+              />
+            </div>
+            <div className="flex-1 aspect-square border-2 ">
+              <img
+                src="/images/single-product-image.jpg"
+                alt="Product Image"
+                className="object-cover w-full h-full "
+              />
+            </div>
+            <div className="flex-1 aspect-square border-2 ">
+              <img
+                src="/images/single-product-image.jpg"
+                alt="Product Image"
+                className="object-cover w-full h-full "
+              />
+            </div>
+          </div>
         </div>
-        <div className="w-full flex flex-col gap-4">
-          <h1 className="font-bold text-2xl">One Life Graphic T-shirt</h1>
+        <div className="w-full flex flex-col gap-6 border-2">
+          <h1 className="font-bold text-2xl lg:text-4xl">
+            One Life Graphic T-shirt
+          </h1>
           <div className="flex items-center gap-1">
             <div className="flex items-center gap-1">
               <Star fill="yellow" color="yellow" />
@@ -52,52 +62,56 @@ export default function ProductDetails() {
               $300
             </span>
           </div>
-          <p className="text-sm opacity-60 leading-5">
+          <p className="text-sm md:text-base opacity-60 leading-5 lg:leading-6">
             This graphic t-shirt which is perfect for any occasion. Crafted from
             a soft and breathable fabric, it offers superior comfort and style.
           </p>
-          <Separator />
+          <div className="w-full h-0.5 bg-gray-200"> </div>
           <div className="flex flex-col gap-4">
-            <span className="text-sm opacity-60">Select Colors</span>
+            <span className="text-sm md:text-base opacity-60">
+              Select Colors
+            </span>
             <div className="flex gap-3 items-center">
-              <span className="p-3 rounded-full bg-gray-500 ">
+              <span className="p-2 rounded-full bg-gray-500 ">
                 <Check color="white" />
               </span>
-              <span className="p-3 rounded-full bg-gray-500 ">
+              <span className="p-2 rounded-full bg-gray-500 ">
                 <Check />
               </span>
-              <span className="p-3 rounded-full bg-gray-500">
+              <span className="p-2 rounded-full bg-gray-500">
                 <Check />
               </span>
             </div>
           </div>
 
-          <Separator />
+          <div className="w-full h-0.5 bg-gray-200"> </div>
+
           <div className="flex flex-col gap-4">
             <span className="text-sm opacity-60">Choose Size</span>
             <div className="flex gap-2">
-              <span className="p-4 rounded-full bg-gray-500    flex items-center justify-center  flex-1">
+              <span className="py-3  rounded-full bg-gray-500  flex items-center justify-center  flex-1">
                 Small
               </span>
-              <span className="flex items-center justify-center    rounded-full bg-gray-500  flex-1">
+              <span className=" py-3 flex items-center justify-center  rounded-full bg-gray-500  flex-1">
                 Medium
               </span>
-              <span className=" rounded-full bg-gray-500 flex    items-center justify-center flex-1">
+              <span className=" py-3  rounded-full bg-gray-500 flex items-center justify-center flex-1">
                 Large
               </span>
-              <span className=" rounded-full bg-gray-500 flex    items-center justify-center  flex-1">
-                Large
+              <span className=" py-3  rounded-full bg-gray-500 flex items-center justify-center  flex-1">
+                X-Large
               </span>
             </div>
           </div>
-          <Separator />
-          <div className="flex gap-3 ">
-            <div className="flex-1 bg-gray-400 px-4 flex gap-1 items-center py-3 rounded-full">
+          <div className="w-full h-0.5 bg-gray-200"></div>
+
+          <div className="flex gap-3">
+            <div className="flex-1 bg-gray-400 px-4 flex  justify-between gap-3 items-center py-3 rounded-full">
               <button>-</button>
               <span>1</span>
               <button>+</button>
             </div>
-            <button className="flex-2 bg-black py-3 text-white rounded-full">
+            <button className="flex-[2] bg-black py-3 text-white rounded-full">
               Add to cart
             </button>
           </div>
